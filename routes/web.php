4def2dbf175afc36d05fc5f502a0bd0017847700
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-11-01 01:09:47 -0400

    menu today, slack msg

--- routes/web.php
+++ routes/web.php
@@ -12,50 +12,109 @@
 */
 use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::post('', function(Request $req){
-
-	// return $req->get('name');
-	// return 'hoanganh';
+Route::get('', function(Request $req){
 	$requestInfo = json_encode($req->all());
-
-	// $curl = curl_init();
-
-	// curl_setopt_array($curl, array(
-	//   CURLOPT_URL => "https://hooks.slack.com/commands/1234/5678",
-	//   CURLOPT_RETURNTRANSFER => true,
-	//   CURLOPT_ENCODING => "",
-	//   CURLOPT_MAXREDIRS => 10,
-	//   CURLOPT_TIMEOUT => 30,
-	//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-	//   CURLOPT_CUSTOMREQUEST => "POST",
-	//   CURLOPT_POSTFIELDS => "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"token\"\r\n\r\no9G92qIXH1WPrIufVfNYIGf9\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"text\"\r\n\r\n{$requestInfo}\r\n-----011000010111000001101001--",
-	//   CURLOPT_HTTPHEADER => array(
-	//     "cache-control: no-cache",
-	//     "content-type: multipart/form-data; boundary=---011000010111000001101001",
-	//     "postman-token: f3b3fa12-7bfc-185b-a748-0c98b250b3db"
-	//   ),
-	// ));
-
-	// $response = curl_exec($curl);
-	// $err = curl_error($curl);
-
-	// curl_close($curl);
-
-	// Handle log
-	$reqLog = fopen('req.log', 'a');
-	fwrite($reqLog, $requestInfo.PHP_EOL);
-	// if ($err) {
-	//   // echo "cURL Error #:" . $err;
-	//   fwrite($reqLog, $err.PHP_EOL);
-	// } else {
-	//   // echo $response;
-	//   fwrite($reqLog, $response.PHP_EOL);
+	// {
+	// 	"token":"o9G92qIXH1WPrIufVfNYIGf9",
+	// 	"team_id":"T0267LQRD",
+	// 	"team_domain":"originally",
+	// 	"channel_id":"D276QQFKN",
+	// 	"channel_name":"directmessage",
+	// 	"user_id":"U276TC8K0",
+	// 	"user_name":"hoanganh25991",
+	// 	"command":"\/menu",
+	// 	"text":"",
+	// 	"response_url":"https:\/\/hooks.slack.com\/commands\/T0267LQRD\/98689833444\/6sC0UlMGrthOD5kr17zbfcsl"
 	// }
+	// // Handle log
+	$reqLog = fopen('req.log', 'a');
+	fwrite($reqLog, date('Y-m-dd').$requestInfo.PHP_EOL);
+	// fwrite($reqLog, date('Y-m-dd').'this from GET'.PHP_EOL);
 	fclose($reqLog);
 
-	return response(['text' => $requestInfo], 200, ['Content-Type' => 'application/json']);
+	$userName = $req->get('user_name');
+	$userText = $req->get('text');
+	// Parse user text into date
+	// If not found $userText = today
+	if(empty($userText) || false){
+		// $userText = date('d');
+		$userText = 'today';
+	}
+
+	$menusFileCache = base_path('menus.json');
+	$menus = json_decode(file_get_contents($menusFileCache), true);
+
+	// Check menu find out menu[0]
+	$menu = $menus[0];
+
+	$dishes = collect($menu['dishes']);
+	$dishes = $dishes->map(function($dish){
+		$tmp = [
+			'title' => $dish['name'],
+			'value' => $dish['price'],
+			'short' => true
+		];
+
+		return $tmp;
+	});
+
+	$slackMsg = [
+		'text' => "Hi, @{$userName}, menu for {$userText}",
+		'attachments' => [
+			[
+				'text' => 'Quan Chanh Cam Tuyet',
+				'fields' => $dishes
+			]
+		]	
+	];
+
+	$reqLog = fopen('req.log', 'a');
+	fwrite($reqLog, date('Y-m-dd').json_encode($slackMsg).PHP_EOL);
+	// fwrite($reqLog, date('Y-m-dd').'this from GET'.PHP_EOL);
+	fclose($reqLog);
+
+	// {
+	// 	"text": "Jenskin console output",
+	// 	"attachments": [
+	//         {
+	//             "fallback": "ReferenceError - UI is not defined: https://honeybadger.io/path/to/event/",
+	//             "text": "<https://honeybadger.io/path/to/event/|ReferenceError> - UI is not defined",
+	//             "fields": [
+	//                 {
+	//                     "title": "Project",
+	//                     "value": "Awesome Project",
+	//                     "short": true
+	//                 },
+	//                 {
+	//                     "title": "Environment",
+	//                     "value": "production",
+	//                     "short": true
+	//                 }
+	//             ],
+	//             "color": "good"
+	//             // "color": "warning"
+	//         }
+	//     ]
+	// }
+	
+	return response($slackMsg, 200, ['Content-Type' => 'application/json']);
 });
+
+// 2016-11-01
+// @bug POST not work, only GET
+// Slack custom command ONLY SEND OUT GET
+// Route::post('', function(Request $req){
+// 	$requestInfo = json_encode($req->all());
+
+// 	// Handle log
+// 	$reqLog = fopen('req.log', 'a');
+// 	fwrite($reqLog, date('Y-m-dd').$requestInfo.PHP_EOL);
+// 	fclose($reqLog);
+
+// 	return response(['text' => 'hello ban hien'], 200, ['Content-Type' => 'application/json']);
+// });
+
+Route::post('menu', function(Request $req){
+
+});
+
